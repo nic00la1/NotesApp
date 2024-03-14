aebeb785f7651e278cda123597d7684b10ff0123
nic00la <nic00la.kaleta@gmail.com>
2024-03-14 18:00:24 +0100

    Adding notes ✅

--- NotesApp/src/app/notes-taking/notes-taking.component.ts
+++ NotesApp/src/app/notes-taking/notes-taking.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { Note } from '../note.model';
 
 @Component({
   selector: 'app-notes-taking',
@@ -11,16 +12,15 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './notes-taking.component.css',
 })
 export class NotesTakingComponent {
-
+  notes: Note[] = [];
+  
   addNote(title: string, content: string) {
-    // Add a new note to the notes array
-
-    if (!title || !content) {
-      alert('Wszystkie pola muszą być wypełnione!');
-      return;
-    }
-
+    const id = this.notes.length + 1; // or however you generate ids
+    const date = new Date();
+    const newNote = new Note(id, title, content, date);
     
-
+    // Add the new note to the list
+    this.notes.push(newNote);
   }
 }
+
